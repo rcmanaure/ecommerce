0c91958fcbc580ee9c20e0629ef557cd121d9f1d
Rcmanaure <rcmanaure@gmail.com>
2020-08-31 18:36:19 -0400

    Add to Cart Functionality. Checkout, payment,shipping address, authentication and others.

--- app/store/urls.py
+++ app/store/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', store_views.store, name='store'),
     path('cart/', store_views.cart, name='cart'),
     path('checkout/', store_views.checkout, name='checkout'),
+    path('update_item/', store_views.updateItem, name='update_item'),
+    path('process_order/', store_views.processOrder, name='process_order'),
 ]
